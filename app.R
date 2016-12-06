452ecbbf5b446b113f6be60d45586fe804e78c25
Gloria Liu <gloriafire96@gmail.com>
2016-12-06 13:51:36 -0800

    tabs

--- app.R
+++ app.R
@@ -5,9 +5,38 @@ library(rsconnect)
 library(ggplot2)
 library(shiny)
 
-ui <- fluidPage()
+ui <- fluidPage(
+  tabsetPanel(
+    tabPanel("Tab1", fluidRow(plotOutput("nationPlot"))),
+    tabPanel("Tab2",
+             sidebarPanel("side panel"),
+             mainPanel("main panel"),
+             fluidRow(plotOutput("statePlot"),
+                      
+                      wellPanel(
+                        sliderInput(inputId = "nlabels",
+                                    label = "Top n States:",
+                                    min = 1,
+                                    max = 10,
+                                    value = 6,
+                                    step = 1)
+                      )
+             )
+    ),
+    tabPanel("Tab3",
+             fluidRow(plotOutput("iStatePlot"),
+                      wellPanel(
+                        htmlOutput("selectState"))
+             )
+    )
+  )
+  
+)
+
+
 
 server <- function(input,output) {
+  
   
 }
 
